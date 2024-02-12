1153198e1b3bb9cb53c7fe3b39179b066bf04745
QiuBingCheng <bingchengqiu@protonmail.com>
2024-02-12 12:37:15 +0800

    Disable raw mode at exit

--- kilo.c
+++ kilo.c
@@ -1,12 +1,22 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 void enableRawMode()
 {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -16,6 +26,15 @@ int main()
 
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-        ;
+    {
+        if (iscntrl(c))
+        {
+            printf("%d\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\n", c, c)
+        }
+    };
     return 0;
 }
