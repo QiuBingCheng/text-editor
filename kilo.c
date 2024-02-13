7e7bc404c497f6cc1bdaeba424668ff7777ce49a
QiuBingCheng <bingchengqiu@protonmail.com>
2024-02-13 20:23:05 +0800

    update some flag and add timeout for read()

--- kilo.c
+++ kilo.c
@@ -17,8 +17,12 @@ void enableRawMode()
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -26,17 +30,22 @@ int main()
 {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    char c = '\0';
+
+    while (1)
     {
+        read(STDIN_FILENO, &c, 1);
         if (iscntrl(c))
         {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
         else
         {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
+
+        if (c == 'q')
+            break;
     };
     return 0;
 }
