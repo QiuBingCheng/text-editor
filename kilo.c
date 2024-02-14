a3a62a7cc2499696e3131184f1af231bd703a4f2
QiuBingCheng <bingchengqiu@protonmail.com>
2024-02-14 14:12:11 +0800

    set up a global struct to store window size

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <termios.h>
+#include <sys/ioctl.h>
 #include <stdlib.h>
 #include <ctype.h>
 
@@ -11,8 +12,14 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
+struct editorConfig
+{
+    struct termios orig_termios;
+    int screenrows;
+    int screencols;
+}
 
-struct termios orig_termios;
+struct editorConfig E;
 
 /*** terminal ***/
 
@@ -26,18 +33,18 @@ void die(const char *s)
 }
 void disableRawMode()
 {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcgetattr");
 }
 
 void enableRawMode()
 {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
         die("tcgetattr");
 
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -74,19 +81,50 @@ void editorProcessKeypress()
     }
 }
 
+void getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        return -1
+    }
+
+    *rows = ws.ws_row;
+    *cols = ws.ws_col;
+    return 0;
+}
 /*** output ***/
+void editorDrawRows()
+{
+    int y;
+    for (y = 0; y < E.screenrows; y++)
+    {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void editorRefreshScreen()
 {
     // Clear the entire screen & Move the cursor to left top
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** init ***/
 
+void initEditor()
+{
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+        die("getWindowSize");
+}
+
 int main()
 {
     enableRawMode();
+    initEditor();
 
     while (1)
     {
