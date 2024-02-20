989fc97651c404a33a159fb7da4dfabe4006b92c
QiuBingCheng <bingchengqiu@protonmail.com>
2024-02-20 22:00:57 +0800

    Add  page up, down, home and end key

--- kilo.c
+++ kilo.c
@@ -21,7 +21,11 @@ enum editorKey
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN,
+    HOME_KEY,
+    END_KEY
 };
 /*** data ***/
 struct editorConfig
@@ -114,19 +118,60 @@ int editorReadKey()
 
         if (seq[0] == '[')
         {
+            if (seq[1] > '0' && seq[1] < '9')
+            {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+
+                if (seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                    case '1':
+                        return HOME_KEY;
+                    case '4':
+                        return END_KEY;
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    case '7':
+                        return HOME_KEY;
+                    case '8':
+                        return END_KEY;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
+                }
+            }
+            return '\x1b';
+        }
+        else if (seq[0] == 'O')
+        {
             switch (seq[1])
             {
-            case 'A':
-                return ARROW_UP;
-            case 'B':
-                return ARROW_DOWN;
-            case 'C':
-                return ARROW_RIGHT;
-            case 'D':
-                return ARROW_LEFT;
+            case 'H':
+                return HOME_KEY;
+            case 'F':
+                return END_KEY;
             }
         }
-        return '\x1b';
     }
     return c;
 }
@@ -135,6 +180,12 @@ void editorMoveCursor(int key)
 {
     switch (key)
     {
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+    case END_KEY:
+        E.cx = E.screencols - 1;
+        break;
     case ARROW_LEFT:
         if (E.cx != 0)
             E.cx--;
@@ -156,6 +207,7 @@ void editorMoveCursor(int key)
 void editorProcessKeypress()
 {
     int c = editorReadKey();
+    int time = E.screenrows;
 
     switch (c)
     {
@@ -165,6 +217,13 @@ void editorProcessKeypress()
         exit(0);
         break;
 
+    case PAGE_UP:
+    case PAGE_DOWN:
+        while (time--)
+        {
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+        break;
     case ARROW_LEFT:
     case ARROW_RIGHT:
     case ARROW_UP:
